934268b0f9a982f650db11f345a2293f994713fd
Diogo Henrique Souza de Lima <91766271+tarcisio-14@users.noreply.github.com>
2023-05-26 21:55:37 -0300

    Create dados.php

---
+++ dados.php
@@ -0,0 +1,68 @@
+<?php
+$produtos = [
+    [
+        'nome' => 'Munchies Classic',
+        'preco' => 25.00,
+        'descricao' => 'Double Chicken Burguer, Queijo Cheedar, Salada.',
+        'imagem' => 'produtos/burg1.jpg',
+        'iden' => 'hbg'
+    ],
+
+    [
+        'nome' => 'Pizza de Peperoni',
+        'preco' => 55.00,
+        'descricao' => 'Pizza tamanho família, 12 fatias, Queijo, Calabresa.',
+        'imagem' => 'produtos/pizza1.jpg',
+        'iden' => 'pizza'
+    ],
+
+    [
+        'nome' => 'Yaksoba Munchies',
+        'preco' => 35.00,
+        'descricao' => 'Lámen, Legumes, Proteína animal, Acompanha shoyu e wasabi.',
+        'imagem' => 'produtos/yaksoba.jpg',
+        'iden' => 'japones'
+    ],
+
+    [
+        'nome' => 'Mini-Combo Sushi',
+        'preco' => 50.00,
+        'descricao' => 'Sushi vegano, Salmão, Sushi tradicional.',
+        'imagem' => 'produtos/sushi1.jpg',
+        'iden' => 'japones'                                                                                                                                                                 
+    ],
+    
+    [
+        'nome' => 'Sashimi Munchies',
+        'preco' => 150.00,
+        'descricao' => 'Sashimi de salmão, Arroz japonês, Ova de peixe.',
+        'imagem' => 'produtos/sushi2.jpg',
+        'iden' => 'japones'                                                                                                                                                                 
+    ],
+    
+    [
+        'nome' => 'X-Bacon Cheedar',
+        'preco' => 30.00,
+        'descricao' => 'Burguer artesanal, Quejo cheedar, Bacon.',
+        'imagem' => 'produtos/bacon.jpg',
+        'iden' => 'hbg'
+    ],
+
+    [
+        'nome' => 'Munchies Vegans',
+        'preco' => 40.00,
+        'descricao' => 'Gorgonzola vegano, Avocado, Alface, Burguer Bean, Tomate ',
+        'imagem' => 'produtos/vegan.jpg',
+        'iden' => 'hbg'
+    ],
+
+    [
+        'nome' => 'Munchies 4 Queijos',
+        'preco' => 55.00,
+        'descricao' => 'Parmesão, Gruyere, Maasdam e Estepe, Pimentão, Azeitona.',
+        'imagem' => 'produtos/pizzaqueijo.jpg',
+        'iden' => 'pizza'
+    ],
+    
+];                      
+?>
